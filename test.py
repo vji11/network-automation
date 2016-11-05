ec437240523f4f3b993c7f813d962a0eb4aee739
vjieanu <valentin.jieanu@gmail.com>
2016-11-05 21:43:22 +0100

    iii

--- test.py
+++ test.py
@@ -12,17 +12,3 @@ First Name: <input type="text" name="first_name">  <br />
 Last Name: <input type="text" name="last_name" />
 <input type="submit" value="Submit" />
 </form>
-
-# Get data from fields
-first_name = form.getvalue('first_name')
-last_name  = form.getvalue('last_name')
-
-print "Content-type:text/html\r\n\r\n"
-print "<html>"
-print "<head>"
-print "<title>Hello - Second CGI Program</title>"
-print "</head>"
-print "<body>"
-print "<h2>Hello %s %s</h2>" % (first_name, last_name)
-print "</body>"
-print "</html>"
