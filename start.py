4e2547893bfd5343c12172f3ac6c1f94f171a058
vjieanu <valentin.jieanu@gmail.com>
2016-11-05 22:42:37 +0100

    ii

--- start.py
+++ start.py
@@ -3,13 +3,11 @@ import cgi
 import cgitb
 cgitb.enable()
 
-
 print 'Content-type: text/html\r\n\r'
 print '<html>'
 print '<h1>Please enter a keyword of your choice</h1>'
-print '<form action="go.py" method="get">'
-print 'First Name: <input type="text" name="first_name">  <br />'
-print 'Last Name: <input type="text" name="last_name" />'
+print '<form action="next.cgi" method="get">'
+print 'Keyword: <input type="text" name="keyword">  <br />'
 print '<input type="submit" value="Submit" />'
 print '</form>'
 print '</html>'
