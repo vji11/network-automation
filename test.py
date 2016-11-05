fa857118c3beb7569d1a2f0701cfb883eb0e45e7
vjieanu <valentin.jieanu@gmail.com>
2016-11-05 21:33:50 +0100

    insert testing

--- test.py
+++ test.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python
 
-md5sum = 'iapula12345'
+# Import modules for CGI handling 
+import cgi, cgitb 
 
-print "md5 " + md5sum + " " + "checksum verified. Upload Succesfull."
+# Create instance of FieldStorage 
+form = cgi.FieldStorage() 
+
+# Get data from fields
+first_name = form.getvalue('first_name')
+last_name  = form.getvalue('last_name')
+
+print "Content-type:text/html\r\n\r\n"
+print "<html>"
+print "<head>"
+print "<title>Hello - Second CGI Program</title>"
+print "</head>"
+print "<body>"
+print "<h2>Hello %s %s</h2>" % (first_name, last_name)
+print "</body>"
+print "</html>"
