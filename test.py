40383a8270eac67a4337c7f813acf49f2950021c
vjieanu <valentin.jieanu@gmail.com>
2016-11-05 22:00:21 +0100

    i

--- test.py
+++ test.py
@@ -6,9 +6,16 @@ import cgi, cgitb
 # Create instance of FieldStorage 
 form = cgi.FieldStorage() 
 
-<form action="test.py" method="get">
-First Name: <input type="text" name="first_name">  <br />
+# Get data from fields
+first_name = form.getvalue('first_name')
+last_name  = form.getvalue('last_name')
 
-Last Name: <input type="text" name="last_name" />
-<input type="submit" value="Submit" />
-</form>
+print "Content-type:text/html\r\n\r\n"
+print "<html>"
+print "<head>"
+print "<title>Hello - Second CGI Program</title>"
+print "</head>"
+print "<body>"
+print "<h2>Hello %s %s</h2>" % (first_name, last_name)
+print "</body>"
+print "</html>"
