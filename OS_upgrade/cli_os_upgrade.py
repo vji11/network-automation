97052a2a7080c76aad69d278e3cfd50f6c3a2464
Jieanu <vjieanu@chellomedia.com>
2016-11-11 12:40:29 +0100

    beta0.5

--- OS_upgrade/cli_os_upgrade.py
+++ OS_upgrade/cli_os_upgrade.py
@@ -4,15 +4,16 @@ import base64
 import time
 
 #variables
-tftp_server = '10.65.18.99'
-my_vrf = 'default'
-cmd_dir = 'dir | i free'
+myuser = raw_input('Username: ')
+mypass = raw_input('Password: ')
+my_vrf = raw_input('VRF: ')
 device = raw_input('Device to upgrade: ')
 image = raw_input('Name of the NX-OS file: ')
 os_size = int(raw_input('Size in bytes of the NX-OS image: '))
 md5_sum = raw_input('MD5 Checksum of the image: ')
-b64pass = base64.b64decode("Y2ljaTE=")
-b64usr = base64.b64decode("dmppZWFudQ==")
+tftp_server = '10.65.18.99'
+
+cmd_dir = 'dir | i free'
 cmd_up = 'copy tftp:'
 cmd_1 = cmd_up + "//" + tftp_server + "/" + image + " " + "bootflash:" + " vrf " + my_vrf
 md5_check = "show file " + image + " " + "md5sum"  
@@ -26,7 +27,7 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 #check if file is already on the flash
 cmd_dir_file = 'dir | i ' + image
 
-ssh.connect(device, port=22, username=b64usr, password=b64pass)
+ssh.connect(device, port=22, username=myuser, password=mypass)
 stdin, stdout, stder = ssh.exec_command(cmd_dir_file)
 output_of_dir_file = stdout.readlines()
 #print '\n'.join(output_of_dir_file)
@@ -40,7 +41,7 @@ else:
     time.sleep(1)
     
 #perform ssh connection
-ssh.connect(device, port=22, username=b64usr, password=b64pass)
+ssh.connect(device, port=22, username=myuser, password=mypass)
 stdin, stdout, stder = ssh.exec_command(cmd_dir)
 output = stdout.readlines()
 ssh.close()
@@ -75,7 +76,7 @@ while True:
         #check disk-space and perform upgrade
         if os_size < bytes_count:
             print "Performing upgrade..."
-            ssh.connect(device, port=22, username=b64usr, password=b64pass)
+            ssh.connect(device, port=22, username=myuser, password=mypass)
             stdin, stdout, stder = ssh.exec_command(cmd_1)
             output1 = stdout.readlines()
             print '\n'.join(output1)
@@ -88,7 +89,7 @@ while True:
     	break
 
 #Verifify the MD5 checksum
-ssh.connect(device, port=22, username=b64usr, password=b64pass)
+ssh.connect(device, port=22, username=myuser, password=mypass)
 stdin, stdout, stder = ssh.exec_command(md5_check)
 output2 = stdout.readlines()
 #print '\n'.join(output2)
