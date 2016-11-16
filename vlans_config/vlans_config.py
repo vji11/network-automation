e895c8eaa8a4230e4d5b4064101e325ab3663e39
Jieanu <vjieanu@chellomedia.com>
2016-11-16 15:27:27 +0100

    iiii

--- vlans_config/vlans_config.py
+++ vlans_config/vlans_config.py
@@ -8,16 +8,14 @@ commands = open('commands.txt', 'r')
 device = open('hosts.txt', 'r')
 verbose = "yes"
 
-# Variables
+
 def variables1():
 	global myuser, mypass
 	myuser = raw_input('Username: ')
 	mypass = getpass.getpass('Password: ')
 
-# Define clear-screen function
 def clear_screen():
 	os.system('cls' if os.name == 'nt' else 'clear')
-
 
 def ssh_connect(commands):
 	global output
@@ -27,7 +25,6 @@ def ssh_connect(commands):
 	ssh.exec_command('terminal length 0\n')
 	stdin, stdout, stder = ssh.exec_command(commands)
 	output = stdout.readlines()
-	print '\n'.join(output)
 	ssh.close()
 
 def config_vlans():
@@ -37,19 +34,15 @@ def config_vlans():
 	print '\n'.join(output)
 	print '\n##### Device Output End #####'	
 
-
-#main program
 def main():
 	print 'Program starting...\n'
 	time.sleep(1)
-	variables1()
-	config_vlans()
+	print device
+	print commands
 
-#run main program in main file
 if __name__ == '__main__':
 	clear_screen()
 	main()
-#end
 
 
 
