acbc4c98d94018a76ef7366860c2828e1aa878b5
Jieanu <vjieanu@chellomedia.com>
2016-11-16 20:25:32 +0100

    iii

--- vlans_config/vlans_config.py
+++ vlans_config/vlans_config.py
@@ -72,8 +72,8 @@ def sh_commands_list():
 	commands = open(commands_file, 'r')
 	print '\n\n\tCommands in file: \n'
 	for x in commands:
-		print '\t\t' + x.strip('\n')
-		print x.strip('\n')
+		#print '\t\t' + x.strip('\n')
+		#print x.strip('\n')
 		cmds_trt = x.strip('\n')
 		print cmds_trt
 	print '\n\n'
